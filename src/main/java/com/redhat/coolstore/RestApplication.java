e4832474e89a7ca444432fecf11106d922c1e6a1
adnan.drina <adnan.drina@gmail.com>
2022-02-16 15:44:03 +0100

    update

--- src/main/java/com/redhat/coolstore/RestApplication.java
+++ src/main/java/com/redhat/coolstore/RestApplication.java
@@ -1,8 +1,12 @@
 package com.redhat.coolstore;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import io.micrometer.core.instrument.MeterRegistry;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -10,5 +14,10 @@ public class RestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RestApplication.class, args);
+    }
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+        return registry -> registry.config().commonTags("application", "catalog-spring-boot");
     }
 }
